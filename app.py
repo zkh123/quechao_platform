997e10b4ba11e3310cfadaa20ad8b9e05ac1281b
zkh123 <849902930@qq.com>
2018-07-22 23:53:08 +0800

    mysql operate

--- app.py
+++ app.py
@@ -8,23 +8,21 @@ reload(sys)
 sys.setdefaultencoding("utf8")
 
 app = Flask(__name__)
-app.config['SECRET_KEY']="1234"
-
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-
+@app.route('/hs')
 def hello():
     return 'ok'
 
-
+# http://127.0.0.1:5000/index
 @app.route('/index')
 def toIndex():
     return render_template('index.html', name='上海')
 
-
+# http://127.0.0.1:5000/add
 @app.route('/add')
 def addData():
     admin = User(username='admin', email='admin@example.com')
@@ -34,6 +32,52 @@ def addData():
     db.session.commit()
     return 'ok'
 
+# http://127.0.0.1:5000/selectAll
+@app.route('/selectAll')
+def select():
+    for user in User.query.all():
+        print('id:{},username:{}, email:{}'.format(user.id, user.username, user.email))
+    return 'ok'
+
+# http://127.0.0.1:5000/selectById/2
+@app.route('/selectById/<int:id>')
+def selectById(id):
+    user = User.query.filter_by(id)
+    print('id:{},username:{}, email:{}'.format(user.id,user.username, user.email))
+    return 'ok'
+
+# http://127.0.0.1:5000/selectByUsername/guest
+@app.route('/selectByUsername/<username>')
+def selectByUsername(username):
+    user = User.query.filter_by(username=username).first()
+    print('id:{},username:{}, email:{}'.format(user.id, user.username, user.email))
+    return 'ok'
+
+# http://127.0.0.1:5000/selectLike
+@app.route('/selectLike')
+def selectLike():
+    list_users = User.query.filter(User.username.endswith('t')).all()
+    for user in list_users:
+        print('id:{},username:{}, email:{}'.format(user.id, user.username, user.email))
+    return 'ok'
+
+
+# http://127.0.0.1:5000/delete
+@app.route('/delete')
+def delete():
+    user = User.query.first()
+    db.session.delete(user)
+    db.session.commit()
+    return 'ok'
+
+
+# http://127.0.0.1:5000/update
+@app.route('/update')
+def update():
+    user = User.query.first()
+    user.username = 'sb'
+    db.session.commit(user)
+    return 'ok'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -8,14 +8,19 @@ reload(sys)
 sys.setdefaultencoding("utf8")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@192.168.1.114:3306/quechao_flask?charset=utf8'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@192.168.1.114:3306/quechao_flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-# app.config['SQLALCHEMY_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
+    def __init__(self,id, username, email):
+        self.id = id
+        self.username = username
+        self.email = email
+
+    """定义数据模型"""
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
